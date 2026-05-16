1dbcc397ae9b465a5b881eb91012648c6ce67242
PAULN2 <paulmartinez1991@gmail.com>
2026-05-16 08:06:30 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Token de tu dispositivo en TagoIO
-const TAGO_TOKEN = "8f28c770-3018-4989-95c2-ecfd0a9f4602";
-
-// API de TagoIO (región US)
-const TAGO_API = "https://api.us.tago.io/data?query=last_value";
-
-// Intervalo de actualización en milisegundos
-const UPDATE_INTERVAL = 5000;
