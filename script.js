4cc732f755ce12617d9503966ccac339c0c61edb
PAULN2 <paulmartinez1991@gmail.com>
2026-05-16 08:55:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 const TAGO_TOKEN = "fdaffbf5-1a1f-40e1-9ba8-f35faf3139c6";
 
 // URL de la API de TagoIO
-const API_URL = "https://api.tago.io/data?query=last_value";
+const API_URL = "https://api.tago.io/data?qty=50";
 
 // =====================================================
 // FUNCIÓN PRINCIPAL
